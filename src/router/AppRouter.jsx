97db8dd3b72a4404668b206c8c00af8d8793557c
Gabriel Cervantes <76570271+gabrielceh@users.noreply.github.com>
2023-06-14 08:56:22 -0500

    Card

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home'
 import { routesName } from '../utils/routes_name'
+import Card from '../components/Card'
 
 const AppRouter = () => {
 	return (
 		<Routes>
 			<Route path={routesName.home} element={<Home />} />
+			<Route path='/card' element={<Card />} />
 		</Routes>
 	)
 }
