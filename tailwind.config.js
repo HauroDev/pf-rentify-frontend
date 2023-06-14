5c152c6fda4c140a150cc24e93a50577f7406b4d
Gabriel Cervantes <76570271+gabrielceh@users.noreply.github.com>
2023-06-14 08:38:57 -0500

    colors in tailwing.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				dark_purple: '#4700D8',
+				medium_purple: '#9900F0',
+				light_purple: '#9900F0',
+				lighter_purple: '#9900F0',
+				text_light: '#333333',
+				body_dark: '#333333',
+				body_light: '#F5F6F8',
+				text_dark: '#F5F6F8',
+				gray_dark: '#999999',
+				gray_medium: '#CCCCCC',
+				purple_badge: '#FFC7F2',
+			},
+		},
 	},
 	plugins: [],
 }
