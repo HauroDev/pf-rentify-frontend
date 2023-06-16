ca7d3b3ae75a2f558dbdd6f22497903e6fc31d47
FabianDP <ingelectronico.fabian@gmail.com>
2023-06-16 14:19:24 -0500

    inicio de crear user

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,1 +1,8 @@
 // servicios user
+import axios from 'axios'
+import { PRODUCTS_API } from '../utils/apiRoutes'
+
+export const getProductByName = async (name) => {
+	const { data } = await axios.post(`${PRODUCTS_API}`)
+	return data
+}
