35ac5e68b7a2c3a2d03a9f58b3f9861a2227565c
FabianDP <ingelectronico.fabian@gmail.com>
2023-06-17 21:05:37 -0500

    validacion con formUsers90%

--- src/components/FormRegister/Form.User.jsx
+++ src/components/FormRegister/Form.User.jsx
@@ -2,153 +2,168 @@ import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { CreatePostUser } from '../../app/features/user/userSlice';
 import Input from '../Input';
+import validation from '../../utils/validation';
 
 const FormUser = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        phone: '',
-        image: '',
-        membership: '',
-        status: '',
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    image: '',
+    membership: '',
+    status: '',
+  });
+  const [error, setError] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    image: '',
+    membership: '',
+    status: '',
+  })
+
+  const handleChange = event => {
+
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value
     });
+    setError(
+      validation({
+        ...formData,
+        [event.target.name]: event.target.value
+      })
+    );
+  }
+  const handleSumit = (event) => {
+    event.preventDefault()
+    console.log(formData)
+    dispatch(CreatePostUser(
+      formData
+    ))
+  }
 
-    const handleChange = event => {
 
-        setFormData({
-            ...formData,
-            [event.target.name]: event.target.value
-        })
-    }
-    const handleSumit = (event) => {
-        event.preventDefault()
-        console.log(formData)
-        dispatch(CreatePostUser(
-            formData
-        ))
-    }
+  return (
+    <div className="relative min-h-screen flex flex-col justify-center items-center bg-gray-100  dark:bg-body_dark" >
+      <div className="relative sm:max-w-sm w-full  dark:bg-body_dark">
 
-    return (
-<div className="relative min-h-screen flex flex-col justify-center items-center bg-gray-100  dark:bg-body_dark" >
-  <div className="relative sm:max-w-sm w-full  dark:bg-body_dark">
-    
-    <form className="shadow-md rounded px-4 sm:px-10 pt-8 pb-4 sm:pt-16 sm:pb-8 mb-4" onSubmit={handleSumit}>
+        <form className="shadow-md rounded px-4 sm:px-10 pt-8 pb-4 sm:pt-16 sm:pb-8 mb-4" onSubmit={handleSumit}>
 
-      <label className="block mt-3 text-sm text-gray-700 text-center font-semibold mb-8">
-        Sign Up
-      </label>
-      
-      <label className="block text-gray-700 text-sm font-bold mb-2">
-        Name:
-      </label>
-      <Input type="text"
-        name="name"
-        placeholder="Enter your name"
-        value={formData.name}
-        onchange={handleChange} />
-      {/* <input
-        type="text"
-        name="name"
-        placeholder="Enter your name"
-        value={formData.name}
-        onChange={handleChange}
-        className="w-full py-2 px-4 text-lg rounded-md border-[1px] border-gray-400 outline-none focus:outline-medium_fuchsia bg-white dark:bg-body_dark"
-      /> */}
+          <h1 className="block mt-3  text-gray-700 text-4xl text-center font-semibold mb-8  dark:text-white">
+            Sign Up
+          </h1>
 
-      <label className="block text-gray-700 text-sm font-bold mb-2">
-        Email:
-      </label>
-      <input
-        type="text"
-        name="email"
-        placeholder="Enter your email"
-        value={formData.email}
-        onChange={handleChange}
-        className="w-full py-2 px-4 text-lg rounded-md border-[1px] border-gray-400 outline-none focus:outline-medium_fuchsia bg-white dark:bg-body_dark"
-      />
+          <label className="block text-gray-700 text-sm font-bold mb-2 dark:text-white">
+            Name:
+          </label>
+          <Input type="text"
+            name="name"
+            placeholder="Enter your name"
+            value={formData.name}
+            onchange={handleChange} />
+          <div className=' text-red-700'>
+            {error.name && <p>{error.name}</p>}
+          </div>
 
-      <label className="block text-gray-700 text-sm font-bold mb-2">
-        Phone:
-      </label>
-      <input
-        type="text"
-        name="phone"
-        placeholder="Enter your phone number"
-        value={formData.phone}
-        onChange={handleChange}
-        className="w-full py-2 px-4 text-lg rounded-md border-[1px] border-gray-400 outline-none focus:outline-medium_fuchsia bg-white dark:bg-body_dark"
-      />
+          <label className="block text-gray-700 text-sm font-bold mb-2 dark:text-white">
+            Email:
+          </label>
+          <Input type="text"
+            name="email"
+            placeholder="Enter your email"
+            value={formData.email}
+            onchange={handleChange} />
+          <div className=' text-red-700'>
+            {error.email && <p>{error.email}</p>}
+          </div>
 
-      <label className="block text-gray-700 text-sm font-bold mb-2">
-        Image:
-      </label>
-      <input
-        type="text"
-        name="image"
-        placeholder="Enter the image URL"
-        value={formData.image}
-        onChange={handleChange}
-        className="w-full py-2 px-4 text-lg rounded-md border-[1px] border-gray-400 outline-none focus:outline-medium_fuchsia bg-white dark:bg-body_dark"
-      />
+          <label className="block text-gray-700 text-sm font-bold mb-2 dark:text-white">
+            Phone:
+          </label>
+          <Input
+            type="text"
+            name="phone"
+            placeholder="Enter your phone number"
+            value={formData.phone}
+            onchange={handleChange}
+          />
+          <div className=' text-red-700'>
+            {error.phone && <p>{error.phone}</p>}
+          </div>
 
-      <label className="block text-gray-700 text-sm font-bold mb-2">
-        Membership:
-      </label>
-      <input
-        type="text"
-        name="membership"
-        placeholder="Enter your membership"
-        value={formData.membership}
-        onChange={handleChange}
-        className="w-full py-2 px-4 text-lg rounded-md border-[1px] border-gray-400 outline-none focus:outline-medium_fuchsia bg-white dark:bg-body_dark"
-      />
+          <label className="block text-gray-700 text-sm font-bold mb-2 dark:text-white">
+            Image:
+          </label>
+          <Input
+            type="text"
+            name="image"
+            placeholder="Enter the image URL"
+            value={formData.image}
+            onchange={handleChange}
+          />
+          <div className=' text-red-700'>
+            {error.image && <p>{error.image}</p>}
+          </div>
 
-      <label className="block text-gray-700 text-sm font-bold mb-2">
-        Status:
-      </label>
-      <input
-        type="text"
-        name="status"
-        placeholder="Enter your status"
-        value={formData.status}
-        onChange={handleChange}
-        className="w-full py-2 px-4 text-lg rounded-md border-[1px] border-gray-400 outline-none focus:outline-medium_fuchsia bg-white dark:bg-body_dark"
-      />
+          <label className="block text-gray-700 text-sm font-bold mb-2 dark:text-white">
+            Membership:
+          </label>
+          <Input
+            type="text"
+            name="membership"
+            placeholder="Enter your membership"
+            value={formData.membership}
+            onchange={handleChange}
+          />
+ 
 
-      <div className="flex mt-7 justify-center w-full">
-        <button
-          type="submit"
-          className="bg-blue-700 border-none px-4 py-2 rounded-xl cursor-pointer text-white shadow-xl hover:shadow-inner transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-105">
-          Sing up
-        </button>
+          <label className="block text-gray-700 text-sm font-bold mb-2 dark:text-white">
+            Status:
+          </label>
+          <Input
+            type="text"
+            name="status"
+            placeholder="Enter your status"
+            value={formData.status}
+            onchange={handleChange}
+          />
+
+
+          <div className="flex mt-7 justify-center w-full">
+            <button
+              type="submit"
+              className="bg-blue-700 border-none px-4 py-2 rounded-xl cursor-pointer text-white shadow-xl hover:shadow-inner transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-105">
+              Sing up
+            </button>
+          </div>
+
+          <div className="bg-gray-300 w-full my-4 py-[1px] rounded-md "></div>
+
+          <label className="block font-medium text-sm text-gray-600 w-full">
+            Or Sing up with
+          </label>
+
+          <div className="flex mt-7 justify-center w-full">
+            <button className="bg-red-500 border-none px-4 py-2 rounded-xl cursor-pointer text-white shadow-xl hover:shadow-inner transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-105">
+              Google
+            </button>
+          </div>
+
+          <div className="mt-7">
+            <div className="flex justify-center items-center">
+              <label className="mr-2">¿Do you have account?</label>
+              <a href="#" className="text-blue-500 transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-105">
+                Login In
+              </a>
+            </div>
+          </div>
+        </form>
       </div>
-      
-      <div className="bg-gray-300  w-full py-[1px] rounded-md "></div> 
-
-      <label className="block font-medium text-sm text-gray-600 w-full">
-        Or Sing up with
-      </label>
-
-      <div className="flex mt-7 justify-center w-full">
-        <button className="bg-red-500 border-none px-4 py-2 rounded-xl cursor-pointer text-white shadow-xl hover:shadow-inner transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-105">
-          Google
-        </button>
-      </div>
-
-      <div className="mt-7">
-        <div className="flex justify-center items-center">
-          <label className="mr-2">¿Do you have account?</label>
-          <a href="#" className="text-blue-500 transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-105">
-            Login In
-          </a>
-        </div>
-      </div>
-    </form>
-  </div>
-</div>
-    )
+    </div>
+  )
 }
 
 
---
+++ src/utils/validation.js
@@ -0,0 +1,30 @@
+const validation = (formData) => {
+    let errors = {};
+  
+    if (!formData.name) {
+      errors.name = 'Please enter a name ';
+    } else if (!/^[a-zA-Z\s]+$/.test(formData.name)) {
+      errors.name = 'Name can only contain letters';
+    }
+  
+    if (!formData.email) {
+      errors.email = 'Please enter an email';
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      errors.email = 'Please enter a valid email address';
+    }
+  
+    if (!formData.phone) {
+      errors.phone = 'Please enter a phone number';
+    } else if (!/^\d{10}$/.test(formData.phone)) {
+      errors.phone = 'Please enter a valid phone number';
+    }
+  
+    // if (!formData.image) {
+    //   errors.image = 'Please enter an image URL';
+    // } else if (!/\.(jpg|jpeg|png|gif)$/i.test(formData.image)) {
+    //   errors.image = 'Please enter a valid image URL';
+    // }
+    return errors;
+  };
+  
+  export default validation;
