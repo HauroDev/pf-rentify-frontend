566729b11f6aabe990ec789588795984ae145d38
FabianDP <ingelectronico.fabian@gmail.com>
2023-06-24 18:37:07 -0500

    todo

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,32 +1,39 @@
-const UserProfile = () => {
-    return (
-      <div className="min-h-screen flex flex-col lg:flex-row justify-between">
-        <div className="w-full lg:w-1/3 border border-gray-300 rounded-lg m-4 lg:m-0">
-          <h2 className="text-xl text-center p-12 font-bold">Imagen</h2>
-          <div>
-            <h1 className="text-xl text-center p-4 font-bold">
-              Wilson Fabian Delgado Pachon
-            </h1>
-          </div>
-          <div>
-            <h3 className="text-xl text-center p-1 font-bold">Phone</h3>
-          </div>
-          <div>
-            <h2 className="text-xl text-center p-2 font-bold">Membership</h2>
-          </div>
-  
-          <div className="text-center py-6">
-            <button className="bg-blue-500 text-white rounded-md px-6 py-1 hover:shadow-inner transition duration-500 ease-in-out transform hover:-translate-x hover:scale-110">
-              Edit Profile
-            </button>
-          </div>
+import PremiumIcon from "../icons/PremiumIcon";
+const UserProfile = ({ image, name, phone, membership }) => {
+  return (
+    <div className="min-h-screen flex flex-col lg:flex-row justify-between">
+      <div className="w-full lg:w-1/3 bg-white dark:bg-card_dark border border-gray-300 rounded-lg m-4 lg:m-0 flex flex-col items-center">
+        <div className="flex justify-center items-center mt-12">
+          <img
+            src={image}
+            alt={name}
+            className="w-48 h-48 rounded-full aspect-w-1 aspect-h-1"
+          />
         </div>
-        <div className="flex-grow border border-gray-300 rounded-lg m-4">
-          <h2 className="text-xl text-center p-12 font-bold">Mis últimos datos</h2>
-          <h1>Listar mis datos</h1>
+        <div className="text-center">
+          <h1 className="text-xl p-4 font-bold">{name}</h1>
+          <h3 className="text-xl p-1 font-bold">Phone: {phone}</h3>
+        </div>
+        <div className="justify-center">
+          <h2 className="text-xl p-2 font-bold flex items-center">
+            Membership: {membership && membership.toUpperCase()}{" "}
+            {membership === "premium" && <PremiumIcon className="w-4 h-4" />}
+          </h2>
+        </div>
+
+        <div className="text-center py-6">
+          <button className="bg-blue-500 text-white rounded-md px-6 py-1 hover:shadow-inner transition duration-500 ease-in-out transform hover:-translate-x hover:scale-110">
+            Edit Profile
+          </button>
         </div>
       </div>
-    );
-  };
-
-export default UserProfile
+      <div className="flex-grow bg-white border dark:bg-card_dark border-gray-300 rounded-lg m-4 flex flex-col justify-center items-center">
+        <h2 className="text-xl p-12 font-bold text-center">
+          Mis últimos datos
+        </h2>
+        <h1>Listar mis datos</h1>
+      </div>
+    </div>
+  );
+};
+export default UserProfile;
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,104 @@
-import UserProfile from "../components/Profile/Profile"
+import UserProfile from "../components/Profile/Profile";
+import { getUser } from "../services/userService";
+import { getUserProducts } from "../services/profile";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Loader from "../components/Loader";
 
+const initalState = {
+  user: {},
+  status: "idle",
+  error: null,
+};
+
+const initalStateProduct = {
+  product: {},
+  status: "idle",
+  error: null,
+};
 const Profile = () => {
-	
-	return (
-       <UserProfile></UserProfile>
-)
+  const [state, setState] = useState(initalState);
+  const [stateProduct, setStateProduct] = useState(initalStateProduct);
+  const { id } = useParams();
+  //USER///////////
+  const getUserId = async (id, set) => {
+    set({
+      status: "loading",
+      user: {},
+      error: null,
+    });
+    try {
+      const data = await getUser(id);
+      console.log(data);
+      set({
+        status: "success",
+        user: { ...data },
+        error: null,
+      });
+    } catch (error) {
+      set({
+        status: "error",
+        user: {},
+        error: error.response.data.message,
+      });
+    }
+  };
 
-}
+  useEffect(() => {
+    getUserId(id, setState);
+    return () => {
+      setState(initalState);
+    };
+  }, [id]);
+  ////////////
+  //PRODUCT///
+  const getProductId = async (id, set) => {
+    set({
+      status: "loading",
+      product: {},
+      error: null,
+    });
+    try {
+      const data = await getUserProducts(id);
+      console.log(data);
+      set({
+        status: "success",
+        product: { ...data },
+        error: null,
+      });
+    } catch (error) {
+      set({
+        status: "error",
+        product: {},
+        error: error.response.data.message,
+      });
+    }
+  };
 
-export default Profile
+  useEffect(() => {
+    getProductId(id, stateProduct);
+    return () => {
+      setStateProduct(initalStateProduct);
+    };
+  }, [id]);
+  ////////
+
+  if (state.status === "loading") return <Loader />;
+
+  if (state.status === "error") return <h3>Error: {state.error}</h3>;
+
+  console.log(state.user);
+
+  return (
+    <>
+      <UserProfile
+        image={state.user.image}
+        name={state.user.name}
+        phone={state.user.phone}
+        membership={state.user.membership}
+      />
+    </>
+  );
+};
+
+export default Profile;
