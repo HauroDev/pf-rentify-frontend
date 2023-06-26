5eb4a6ff568fde3eb56c6ba1e9a011248ee962aa
FabianDP <ingelectronico.fabian@gmail.com>
2023-06-26 18:25:02 -0500

    cambio

---
+++ src/services/profile.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+import { PRODUCTS_API } from "../utils/apiRoutes";
+
+export const getUserProducts = async (id) => {
+  const { data } = await axios.get(`${PRODUCTS_API}/user/${id}`);
+  return data;
+};
