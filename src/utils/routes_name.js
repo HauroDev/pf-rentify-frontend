e19f508017e95c36e748335e0570a36800c24e83
Gonzalo <utngonzalollanos@gmail.com>
2023-06-25 22:38:53 -0300

    routes

--- src/utils/routes_name.js
+++ src/utils/routes_name.js
@@ -7,7 +7,7 @@ export const routesName = {
   user: {
     "create-product": "/user/create-product",
 
-    profile: "/profile/:id",
+    profile: "/user/profile",
   },
   checkout: {
     checkout: "/checkout",
