b302648c22a5a31900b0bfb42fd51b5fec005ca5
Ale <nuala.ni.ale@gmail.com>
2023-06-27 22:57:20 -0500

    añadido how-it-works, pricing

--- src/pages/HowItWork.jsx
+++ src/pages/HowItWork.jsx
@@ -1,5 +1,88 @@
+import laptopYCajitas from '../assets/image/laptop-cajitas.jpg';
+import consola from '../assets/image/consola.jpg';
+import mobile from '../assets/image/mobile.png';
+import shipping from '../assets/image/shipping.png';
+import cicle from '../assets/image/cicle.png';
+import handCoin from '../assets/image/hand-coin.png';
+import calendar from '../assets/image/calendar.png';
+import greenWorld from '../assets/image/green-world.png';
+
+
 const HowItWork = () => {
-	return <div>HowItWork</div>
+	return (
+		<div>
+			<div>
+				<div>
+					<div>
+						<h2>How renting works</h2>
+						<p>Explore Placing an Order, the Advantages of Renting, and Our Commitment to Resource Conservation.</p>
+					</div>
+				</div>
+				<img src={laptopYCajitas} alt="" className='h-64' />
+			</div>
+
+			<div>
+				<div>
+					<p>How it works</p>
+					<h3>Getting started with Rent-ify</h3>
+				</div>
+
+				<div>
+					<div>
+						<p>1</p>
+						<p>Find Your Ideal Rental</p>
+						<p>Explore vast product options. From tech to furniture, we have it all. Discover your perfect match.</p>
+						<img src={consola} alt="consola" />
+					</div>
+					<div>
+						<p>2</p>
+						<p>Checkout</p>
+						<p>Select items and proceed to checkout. Credit check may apply. Get shipping details within 24 hours.</p>
+						<img src={mobile} alt="mobile" />
+					</div>
+					<div>
+						<p>3</p>
+						<p>Enjoy your rental</p>
+						<p>Experience the benefits of your rented items. Fast delivery to your doorstep for maximum enjoyment.</p>
+						<img src={shipping} alt="shipping" />
+					</div>
+					<div>
+						<p>4</p>
+						<p>Sustainable Refurbishment</p>
+						<p>Rent with us and experience convenience, flexibility, and resource conservation. Let's make a positive impact together!</p>
+						<img src={cicle} alt="cicle" />
+					</div>
+				</div>
+			</div>
+
+			<div>
+				<div>
+					<p>Buying is over</p>
+					<h3>The benefits of renting</h3>
+				</div>
+
+				<div>
+					<div>
+						<p>Low Monthly Costs</p>
+						<img src={handCoin} alt="hand-coin" />
+						<p>Say goodbye to high upfront costs and long-term commitments. Rent a wide range of products at budget-friendly prices.</p>
+					</div>
+
+					<div>
+						<p>Flexible Rental Periods</p>
+						<img src={calendar} alt="calendar" />
+						<p></p>
+					</div>
+					
+					<div>
+						<p>Environmental Impact</p>
+						<img src={greenWorld} alt="green-world" />
+						<p>By renting, you help extend the lifespan of products, minimizing unnecessary consumption and promoting sustainability.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default HowItWork
--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -1,5 +1,105 @@
+import social from '../assets/image/social-commerce-image.png';
+import shippingCajita from '../assets/image/shipping-2.png'
+import diamond from '../assets/image/diamond.jpg'
+import userIcon from '../assets/image/user-icon.png';
+import handDiamond from '../assets/image/hand-diamond.png';
+import securePayment from '../assets/image/secure-payment.png';
+import membershipIcon from '../assets/image/membership-icon.png'
+
+import { useSelector } from 'react-redux';
+
+
 const Pricing = () => {
-	return <div>Pricing</div>
+	const {user} = useSelector(state=>state.user)
+	console.log(user);
+	return (
+		<div>
+			<div>
+				<div>
+					<h2>Rent-ify Membership</h2>
+					<h3>Exclusive acces and special benefits</h3>
+				</div>
+				<img src={social} alt="social" />
+			</div>
+
+			<div>
+				<h3>Unlock a world of convenience and savings with our exclusive membership program</h3>
+				<div>
+					<div>
+						<p>Gain unlimited access to a wide range of items, enjoy discounted transaction fees, and receive top-notch support.</p>
+					</div>
+					<img src={shippingCajita} alt="shipping-cajita" />
+					<div>
+						<p>Whether you're a frequent renter or a savvy item owner, our membership offers a host of benefits tailored to enhance your renting experience.</p>
+					</div>
+				</div>
+			</div>
+
+			<div>
+				<div><h3>Membership Plans</h3></div>
+				<div>
+					<div>
+						<p>Standard Membership (Free)</p>
+						<ul>
+							<li>Up to 5 items listings per month.</li>
+							<li>Standard transaction fees.</li>
+							<li>Enjoy Rent-ify for free.</li>
+						</ul>
+						{
+							user?.membership === "premium"
+							? <button>Subscribe</button>
+							: null
+						}
+					</div>
+
+					<div>
+						<p>Premium Membership</p>
+						<ul>
+							<li>Unlimited number of item listings per month.</li>
+							<li>Reduced transaction fees</li>
+							<li>Priority highlighting in search results</li>
+							<li>Premium customer support</li>
+							<li>Price: $X.XX per month</li>
+						</ul>
+						{
+							user?.membership === "standard"
+							? <button>Subscribe</button>
+							: null
+						}
+					</div>
+				</div>
+			</div>
+
+			<div>
+				<div>
+					<p>How to</p>
+					<h3>Subscribe to MemberShip</h3>
+				</div>
+				<div>
+					<div>
+						<p>1</p>
+						<p>Sign up or log in to your Rent-ify account.</p>
+						<img src={userIcon} alt="user-icon" />
+					</div>
+					<div>
+						<p>2</p>
+						<p>Select the desired plan (Premium or Standard).</p>
+						<img src={handDiamond} alt="hand-diamond" />
+					</div>
+					<div>
+						<p>3</p>
+						<p>Complete the secure payment process (if applicable).</p>
+						<img src={securePayment} alt="secure-payment" />
+					</div>
+					<div>
+						<p>4</p>
+						<p>Confirm your membership and gain access to benefits.</p>
+						<img src={membershipIcon} alt="membership-icon" />
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Pricing
