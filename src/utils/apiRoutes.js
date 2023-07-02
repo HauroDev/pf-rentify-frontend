cf82b7e826338ed2b55af376e21506c0151bb165
gonlla8777 <utngonzalollanos@gmail.com>
2023-07-02 19:15:11 -0300

    todo kok

--- src/utils/apiRoutes.js
+++ src/utils/apiRoutes.js
@@ -1,17 +1,17 @@
-const API_URL = 'https://api-rentify.onrender.com/api-rentify'
-// const API_URL = 'http://localhost:3001/api-rentify'
-export const MCP_API = `${API_URL}/payments`
+//const API_URL = 'https://api-rentify.onrender.com/api-rentify'
+const API_URL = "http://localhost:3001/api-rentify";
+export const MCP_API = `${API_URL}/payments`;
 
-export const PRODUCTS_API = `${API_URL}/products`
+export const PRODUCTS_API = `${API_URL}/products`;
 
-export const CATEGORIES_API = `${API_URL}/categories`
+export const CATEGORIES_API = `${API_URL}/categories`;
 
-export const USER_API = `${API_URL}/user`
+export const USER_API = `${API_URL}/user`;
 
-export const COUNTRIES_API = `${API_URL}/countries`
+export const COUNTRIES_API = `${API_URL}/countries`;
 
-export const LOGIN_API = `${API_URL}/login-google`
+export const LOGIN_API = `${API_URL}/login-google`;
 
-export const COMMENT_API = `${API_URL}/comment`
+export const COMMENT_API = `${API_URL}/comment`;
 
-export const FORM_CONTACT_API = `${API_URL}/form-contact`
+export const FORM_CONTACT_API = `${API_URL}/form-contact`;
