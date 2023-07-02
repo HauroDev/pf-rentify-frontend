55512ef3d53bf6009277ef621452c5ae26ce4779
asracedof <116568718+asracedof@users.noreplay.github.com>
2023-07-02 10:00:17 -0500

    FAQS

--- src/pages/FAQS.jsx
+++ src/pages/FAQS.jsx
@@ -2,12 +2,13 @@ import React, { useState, useEffect, useContext } from 'react';
 import { contactContacUsService } from '../services/contactService';
 import { ToastContext } from '../context/ToastContext';
 import { useForm } from 'react-hook-form';
+import { FaPhone, FaEnvelope, FaClock } from 'react-icons/fa';
 
 const FAQS = () => {
   const [activeIndex, setActiveIndex] = useState(null);
   const [showContactForm, setShowContactForm] = useState(false);
   const { register, handleSubmit, reset } = useForm();
-  
+
   const { addToast } = useContext(ToastContext);
 
   useEffect(() => {
@@ -128,7 +129,7 @@ const FAQS = () => {
           </div>
         ))}
       </div>
-      <div className='mt-4 flex flex-col items-center'>
+      <div className='mt-4 flex flex-col items-start'>
         {!showContactForm ? (
           <button
             className='bg-medium_purple text-white text-xl py-2 px-4 rounded hover:bg-dark_purple'
@@ -137,15 +138,24 @@ const FAQS = () => {
             Ask a Question
           </button>
         ) : (
-          <div className='flex flex-row w-full'>
-            <div className='w-1/2 text-center py-8'>
+          <div className='flex flex-row w-full gap-8'>
+            <div className='w-1/2 text-left py-4'>
               <p className='text-2xl'>Hi 👋 !</p>
-              <p className='text-lg mt-4'>
-                If you are unable to find the answer to your question, please don&apos;t hesitate to
-                contact our dedicated support team. Our team is available Monday through Friday,
-                from 9 am to 6 pm (GMT). To get in touch, kindly fill out the contact form below,
-                and we will respond to your inquiry promptly.
+              <p className='text-lg'>
+                If you are unable to find the answer to your question, please contact our support team for any inquiries. We're here to help.
               </p>
+              <div className='flex justify-start mt-4'>
+              <FaClock size={20} />
+              <span className='ml-2'>Monday to Friday, 9 am to 6 pm (GMT)</span>
+            </div>
+              <div className='flex justify-start mt-4'>
+                <FaPhone size={20} />
+                <span className='ml-2'>123-456-7890</span>
+              </div>
+              <div className='flex justify-start mt-2'>
+                <FaEnvelope size={20} />
+                <span className='ml-2'>rent.ify.soporte@gmail.com</span>
+              </div>
             </div>
             <div className='w-1/2'>
               <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col items-center'>
@@ -154,21 +164,21 @@ const FAQS = () => {
                   placeholder='Name'
                   name='name'
                   {...register('name')}
-                  className='w-full px-4 py-2 mb-4 rounded border text-black border-gray_medium focus:outline-none'
+                  className='w-full px-4 py-2 mb-4 border border-gray-300 rounded focus:outline-none focus:ring focus:border-purple-500'
                 />
                 <input
                   type='email'
                   placeholder='Email'
                   name='email'
                   {...register('email')}
-                  className='w-full px-4 py-2 mb-4 rounded border text-black border-gray_medium focus:outline-none'
+                  className='w-full px-4 py-2 mb-4 border border-gray-300 rounded focus:outline-none focus:ring focus:border-purple-500'
                 />
                 <textarea
                   placeholder='Message'
                   rows='4'
                   name='message'
                   {...register('message')}
-                  className='w-full px-4 py-2 mb-4 rounded border text-black border-gray_medium focus:outline-none'
+                  className='w-full px-4 py-2 mb-4 border border-gray-300 rounded focus:outline-none focus:ring focus:border-purple-500'
                 />
                 <button
                   type='submit'
@@ -178,11 +188,14 @@ const FAQS = () => {
                 </button>
               </form>
             </div>
+            
           </div>
         )}
       </div>
     </div>
   );
+  
+  
 };
 
 export default FAQS;
